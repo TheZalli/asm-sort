1a190a5ea882838f370c91701dec3bb22ce79546
mrZalli <misterzalli@gmail.com>
2017-10-06 02:08:58 +0300

    Add inner search loop (untested)

--- sort.s
+++ sort.s
@@ -23,29 +23,37 @@ sort: # the sorting function
     # + <=> -                     (%rbp) -8   -10  -18 (%rsp)
     # stack is now: ret addr, old %rbp | ptr, len, end |
 
-    # iterator/pointer
+    # first iterator/pointer
     movq -0x8(%rbp), %rax
-    loop:
-        movq $0, (%rax) # TODO
+
+    # the loop from start to end
+    main_loop:
+        # second iterator/pointer
+        movq %rax, %rcx
+        # the value
+        movq (%rax), %rdx
+
+        # the insertion loop
+        cmp_loop:
+            subq $0x8, %rcx        # move 2nd iterator to left by quad
+
+            cmpq -0x8(%rbp), %rcx  # check if we are inside bounds
+            jle cmp_loop_break     # if at border (or outside), break
+
+            cmpq %rdx, (%rcx)      # compare with the chosen value
+            jge cmp_loop           # if value is greater than compared, continue
+            # otherwise break
+        cmp_loop_break:
+
+        # 2nd iterator is now on the the place to be inserted
+        # TODO
 
         addq $0x8, %rax  # advance iterator by one quad
-        cmpq -0x18(%rbp), %rax
-    jl loop
+        cmpq -0x18(%rbp), %rax  # check if we are inside bounds
+    jl main_loop
 
     # delete stackframe
     movq %rbp, %rsp
     popq %rbp
-
-    ret
-
-# void shift_insert(int64_t* start, int64_t value, int64_t* end)
-# start (ptr): %rdi
-# value (quad): %rsi
-# end (ptr): %rdx
-#
-# Requires the sort function's input parameters to be in the stack
-shift_insert:
-    # byte count
-    subq %rdi, %rdx
 
     ret
