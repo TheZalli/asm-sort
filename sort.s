f263001726141d0d8356b2df14feec8e19c80bff
mrZalli <misterzalli@gmail.com>
2017-10-05 21:51:33 +0300

    Add loop

--- sort.s
+++ sort.s
@@ -12,32 +12,40 @@ sort: # the sorting function
 
     # stack is now: return address, old stack base pointer
     
-    subq $0x10, %rsp
-    movq %rdi, -0x8(%rbp)
-    movq %rsi, -0x10(%rbp)
+    subq $0x18, %rsp
+    movq %rdi, -0x8(%rbp)  # array ptr
+    movq %rsi, -0x10(%rbp) # array len
 
-    #                                (%rbp)     (%rsp)
-    # stack is now: ret addr, old %rbp | ptr, len |
+    shl $0x3, %rsi  # quad count to byte count, *2^3 <=> *8
+    addq %rsi, %rdi # end pointer, one past last quad
+    movq %rdi, -0x18(%rbp) # array end ptr
 
-    # count
-    shr $0x1, %rsi # div by 2
-    shl $0x3, %rsi # int count to byte count
-    movq %rsi, %rdx
-    
-    # src
-    movq %rdi, %rsi
+    # + <=> -                     (%rbp) -8   -10  -18 (%rsp)
+    # stack is now: ret addr, old %rbp | ptr, len, end |
 
-    # dest
-    addq %rdx, %rdi
+    # iterator/pointer
+    movq -0x8(%rbp), %rax
+    loop:
+        movq $0, (%rax) # TODO
 
-    call memmove
-
-    # set 0th to 2
-    movq -0x8(%rbp), %rdi
-    movq $0x2, (%rdi)
+        addq $0x8, %rax  # advance iterator by one quad
+        cmpq -0x18(%rbp), %rax
+    jl loop
 
     # delete stackframe
     movq %rbp, %rsp
     popq %rbp
+
+    ret
+
+# void shift_insert(int64_t* start, int64_t value, int64_t* end)
+# start (ptr): %rdi
+# value (quad): %rsi
+# end (ptr): %rdx
+#
+# Requires the sort function's input parameters to be in the stack
+shift_insert:
+    # byte count
+    subq %rdi, %rdx
 
     ret
